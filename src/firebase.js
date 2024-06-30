116c1ae154742246678805b5a14ac5a7148e2c55
suneet04 <136145763+suneet04@users.noreply.github.com>
2024-06-30 22:47:28 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyCO426flhc9m691ChimsOaR9FuqXx5C6qk",
+  authDomain: "rajat-chatapp.firebaseapp.com",
+  projectId: "rajat-chatapp",
+  storageBucket: "rajat-chatapp.appspot.com",
+  messagingSenderId: "621292462018",
+  appId: "1:621292462018:web:2f4fd108da442a1dd1c05b"
+};
+
+export const app = initializeApp(firebaseConfig);
